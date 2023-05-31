114a9839abc248867f898c5c66cc915f58c22957
Wiran_Larbi <71338128+Wiran-Larbi@users.noreply.github.com>
2023-05-31 18:59:52 +0100

    Initial commit

---
+++ src/main/java/com/groupyfy/groupyfy/GroupyFyApplication.java
@@ -0,0 +1,13 @@
+package com.groupyfy.groupyfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupyFyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupyFyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupyfy/groupyfy/GroupyFyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupyfy.groupyfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupyFyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
